80ee94f3f4a7baab2f20bafc9b8f590d2df737b5
jcgarsan <jc.garsan@gmail.com>
2015-02-21 20:55:03 +0100

    Search the joystick, get data and publish the topic

--- src/ros-uji-joystick.py
+++ src/ros-uji-joystick.py
@@ -1,53 +1,98 @@
+#!/usr/bin/env python
+# coding:utf8
+
+"""
+@package ros-uji-joystick
+@file ros-uji-joystick.py
+
+@brief Joystick generic driver for ROS.
+
+@author: Juan Carlos García <garciaju@uji.es>
+@date 18/02/2015
+"""
+
 import pygame
 import sys
+import signal
 import time
+import rospy
+from sensor_msgs.msg import Joy
 
+# Initialize consts and vars
+joy_pub = rospy.Publisher('joystick_out', Joy, queue_size=10)
+rospy.init_node('ros_uji_joystick', anonymous=True)   # Initialize node
+rospy.Rate(50);
 pygame.init()
 pygame.joystick.init()
 
-print pygame.joystick.get_count()
-_joystick = pygame.joystick.Joystick(0)
-_joystick.init()
+#Debug flag: show human-readable data on terminal
+DEBUG = False
 
-clock = pygame.time.Clock()
-
-print _joystick.get_init()
-print _joystick.get_id()
-print _joystick.get_name()		#Logitech Logitech Extreme 3D
-print _joystick.get_numaxes()
-print _joystick.get_numballs()
-print _joystick.get_numbuttons()
-print _joystick.get_numhats()
-#	print _joystick.get_hat(0)
+def signal_handler(signal, frame):
+	print "\nClosing the program..."
+	sys.exit(0)
 
 
-while 1:
-    for event in pygame.event.get(): # User did something
-        # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
-        if event.type == pygame.JOYBUTTONDOWN:
-            print("Joystick button pressed.")
-        if event.type == pygame.JOYBUTTONUP:
-            print("Joystick button released.")
-        if event.type == pygame.JOYAXISMOTION:
-            print("Joystick JOYAXISMOTION.")
+def main():
+	joy_msg = Joy()
+	joy_msg.header.stamp = rospy.Time.now()
+	joyId = 0
+	joyDetected = False
+	quitting = False
+	
+	signal.signal(signal.SIGINT, signal_handler)
+	clock = pygame.time.Clock()
 
-	print _joystick.get_axis(0)
-	print _joystick.get_button(0)
+	#Search our joystick if there is more than one
+	joystick = pygame.joystick.Joystick(0)
+	joystick.init()
+	while joyId <= pygame.joystick.get_count() and not joyDetected:
+		if joystick.get_name() == "Logitech Logitech Extreme 3D":
+			joyDetected = True
+			print "Joystick detected"
+		else:
+			joyId += 1
+			joystick = pygame.joystick.Joystick(joyId)
+			joystick.init()
 
-	buttons = _joystick.get_numbuttons()
-	for i in range(buttons):
-		button = _joystick.get_button(i)
-		print "Button {:>2} value: {}".format(i,button)
+	#Get the number of axes/numbers and initialize joy_msg
+	joyAxes = joystick.get_numaxes()
+	joyButtons = joystick.get_numbuttons()
+	joy_msg.axes = [0.0] * joyAxes
+	joy_msg.buttons = [0.0] * joyButtons
+	print "Number of axes: {}".format(joyAxes)
+	print "Number of buttons: {}".format(joyButtons)
 
-	axes = _joystick.get_numaxes()
-	print "Number of axes: {}".format(axes)
+	raw_input('Press "Enter" to continue...')
 
-	for i in range( axes ):
-		axis = _joystick.get_axis( i )
-		print "Axis {} value: {:>6.3f}".format(i, axis)
+	#Get the data, create the message and publish it
+	while not quitting:
+		try:
+			pygame.event.get()
 
+			for i in range(joyAxes):
+				joy_msg.axes[i] = joystick.get_axis(i)
+				if DEBUG:
+					print "Axis {} value: {:>6.3f}".format(i, joystick.get_axis(i))
+			for i in range(joyButtons):
+				joy_msg.buttons[i] = joystick.get_button(i)
+				if DEBUG:
+					print "Button {:>2} value: {}".format(i, joystick.get_button(i))
 
-#	clock.tick(20)
+			#Publish the data
+			joy_pub.publish(joy_msg)
 
+			if DEBUG:
+				clock.tick(20)
+				print "Axes:"
+				print joy_msg.axes
+				print "Buttons:"
+				print joy_msg.buttons
 
+		except (KeyboardInterrupt, SystemExit):
+			quitting = True
+			print "KeyboardInterrupt detected..."
+
+if __name__ == '__main__':
+	main()
 
